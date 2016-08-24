dd75474261727e291bfdea7c5479f92e54740b5a
Ari Johannesson <arividar@gmail.com>
2016-08-24 09:22:03 +0000

    Initial commit

---
+++ TheApp/TheApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TheApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnCrashButtonClicked(object sender, EventArgs args)
+        {
+            throw new Exception("Hey! I am not handled by anyone!");
+        }
+    }
+}
